4975f7c3f0d2a253d010fd985a811f3a5f611864
Martin Johnson <mdj128@gmail.com>
2019-12-31 15:01:47 -0500

    some rudimentary engine formulas

--- index.js
+++ index.js
@@ -1,37 +1,62 @@
 
-const p1 = { name: 'Spartacus', lvl: 1, hp: 30, str: 5 };
-const p2 = { name: 'Gannicus', lvl: 1, hp: 30, str: 5 };
+const p1 = { name: 'Spartacus', lvl: 10, maxhp: 300, hp: 300, str: 20, dex: 10, agi: 15, weapon: 10, ac: 10 };
+const p2 = { name: 'Gannicus', lvl: 10, maxhp: 300, hp: 300, str: 5, dex: 5, agi: 10, weapon: 10, ac: 30 };
 
-while (p1.hp > 0 && p2.hp > 0) {
-  attackRound(p1, p2);
-  if (p2.hp <= 0) {
-    console.log(`${p1.name} has defeated ${p2.name}!`);
-    break;
+const fight = (p1, p2) => {
+  while (p1.hp > 0 && p2.hp > 0) {
+    attackRound(p1, p2);
+    if (p2.hp <= 0) {
+      // console.log(`${p1.name} has defeated ${p2.name}!`);
+      break;
+    }
+    attackRound(p2, p1);
+    if (p1.hp <= 0) {
+      // console.log(`${p2.name} has defeated ${p1.name}!`);
+      break;
+    }
   }
-  attackRound(p2, p1);
-  if (p1.hp <= 0) {
-    console.log(`${p2.name} has defeated ${p1.name}!`);
-    break;
+};
+
+let p1wins = 0;
+
+for (let i = 0; i < 10000; i++) {
+  fight(p1, p2);
+  if (p1.hp > 0) {
+    p1wins++; 
   }
+  p1.hp = p1.maxhp;
+  p2.hp = p2.maxhp;
 }
+
+console.log(`${p1.name} won ${Math.round((p1wins / 10000) * 100)}% of the time.`);
+
 
 function attackRound(p1, p2) {
   const d1 = calculateDamage(p1, p2);
-  console.log(d1 === 0 ? `${p1.name} tried to hit ${p2.name}, but missed!` : `${p1.name} hit ${p2.name} for ${d1} damage!`);
+  // console.log(d1 === 0 ? `${p1.name} tried to hit ${p2.name}, but missed!` : `${p1.name} hit ${p2.name} for ${d1} damage!`);
   p2.hp -= d1;
 }
 
 function calculateDamage(attacker, defender) {
-  let damage = Math.round(Math.random() * 10);
-  if (damage < 5) {
-    damage = 0;
+  // first roll for miss based on agi and dex
+  const missPct = 0.1980 - 0.003790 * attacker.dex + 0.004323 * defender.agi;
+  if (Math.random() <= missPct) {
+    return 0;
   }
-  return damage;
+
+  const weaponMod	=	7.928 + 0.14210 * attacker.weapon;
+  const strVsAcMod = 1.0070 + 0.011422 * attacker.str - 0.011120 * defender.ac;
+  const base = Math.random() * weaponMod; 
+
+  // apply modifier based on regression model for strength
+  const damage = base * strVsAcMod;
+
+  return Math.round(damage);
 }
 
 // Normal distribution with min, max, skew
 // https://jsfiddle.net/ktq9jaoe/4/
-function randn_bm(min, max, skew) {
+function randn_bm(min, max, skew, whole = true) {
   let u = 0, v = 0;
   while (u === 0) {
     u = Math.random();
@@ -48,5 +73,5 @@ function randn_bm(min, max, skew) {
   num = Math.pow(num, skew); // Skew
   num *= max - min; // Stretch to fill range
   num += min; // offset to min
-  return num;
+  return whole ? Math.round(num) : num;
 }
